c470ea9fe41a554285aee8af53b6b45032cf9afc
Robert Gleason <l2ob122@gmail.com>
2023-12-14 12:47:24 -0800

    working with db setup and fetched data

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,8 +1,37 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { db } from '../utils/db.server';
+import { Link, useLoaderData } from '@remix-run/react';
+
+// This is a loader function for the Remix route 'jokes.$jokeId.tsx'.
+// It takes an object with route parameters as an argument.
+// It uses the 'jokeId' parameter to fetch a unique joke from the database.
+// If the joke is found, it returns a JSON response with the joke object.
+// If the joke is not found, it returns a JSON response with a 'Joke not found' message and a 404 status code.
+/**
+ * Loads a joke based on the provided jokeId.
+ * @param params - The parameters object containing the jokeId.
+ * @returns A JSON response containing the joke if found, or a 404 error message if not found.
+ */
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) {
+    return json({ message: 'Joke not found' }, { status: 404 });
+  }
+  return json({ joke });
+};
+
 export default function JokeRoute() {
-  return (
-    <div>
-      <p>Another hilarious joke!</p>
-      <p>Knock knock? whos there? No one AHHAHAHAHA</p>
-    </div>
-  );
+  const data = useLoaderData<typeof loader>();
+
+  if ('joke' in data)
+    return (
+      <div>
+        <p>Another hilarious joke!</p>
+        <p>{data.joke.content}</p>
+        <Link to='.'>{data.joke.name} Permalink</Link>
+      </div>
+    );
 }
--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,12 +1,31 @@
-import { Outlet, Link } from '@remix-run/react';
+import { Outlet, Link, useLoaderData } from '@remix-run/react';
 import type { LinksFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { db } from '../utils/db.server';
 
 import stylesUrl from '../styles/jokes.css';
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesUrl },
 ];
+
+/**
+ * Loads the joke list items from the database.
+ * @returns {Promise<{ jokeListItems: Joke[] }>} The joke list items.
+ */
+export const loader = async () => {
+  return json({
+    jokeListItems: await db.joke.findMany({
+      orderBy: { createdAt: 'desc' },
+      select: { id: true, name: true },
+      take: 5,
+    }),
+  });
+};
+
 export default function JokesRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div className='jokes-layout'>
       <header className='jokes-header'>
@@ -25,9 +44,11 @@ export default function JokesRoute() {
             <Link to='.'>Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to='some-joke-id'>Bekekkkkeekk</Link>
-              </li>
+              {data.jokeListItems.map(({ id, name }) => (
+                <li key={id}>
+                  <Link to={id}>{name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to='new' className='button'>
               Add your own
