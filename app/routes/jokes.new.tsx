ee8da604ee115965a76e6ffea6d512dedc80782d
Robert Gleason <l2ob122@gmail.com>
2023-12-14 18:35:52 -0800

    test

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,3 +1,23 @@
+import { authenticator } from "~/services/auth.server";
+import type { ActionFunctionArgs } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { db } from '../utils/db.server';
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  await authenticator.isAuthenticated(request, { failureRedirect: "/login" });
+
+  const form = await request.formData();
+  const content = form.get('content');
+  const name = form.get('name');
+
+  if (typeof content !== 'string' || typeof name !== 'string') {
+    throw new Error('Invalid form submission');
+  }
+
+  const fields = { content, name };
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
 export default function NewJokeRoute() {
   return (
     <div>
@@ -14,7 +34,9 @@ export default function NewJokeRoute() {
           </label>
         </div>
         <div>
-          <button type='submit'>Add</button>
+          <button type='submit' className='button'>
+            Add
+          </button>
         </div>
       </form>
     </div>
